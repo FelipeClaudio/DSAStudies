c31342fbd6e07b9b6ae62d41eef79b4537bd98b1
Felipe Santos <felipesantos956@poli.ufjr.br>
2021-09-30 00:13:30 -0300

    Binary search tree completly working.

--- Trees/BinarySearchTree.cs
+++ Trees/BinarySearchTree.cs
@@ -7,13 +7,13 @@ namespace Trees
         where T : BinarySearchTreeNode<V>
         where V : IComparable
     {
-        public BinarySearchTreeNode<V> GetElementNode(T element)
+        public BinarySearchTreeNode<V> GetElementNodeByValue(V value)
         {
             var currentNode = this.Root;
 
             while (currentNode != null)
             {
-                int comparisionResult = currentNode.Data.CompareTo(element.Data);
+                int comparisionResult = currentNode.Data.CompareTo(value);
                 if (comparisionResult == 0)
                     return currentNode;
                 else if (comparisionResult > 0)
@@ -27,13 +27,13 @@ namespace Trees
 
         public void InsertValue(V elementValue)
         {
+            if (this.GetElementNodeByValue(elementValue) != null)
+                throw new ArgumentException($"Node with value: {elementValue} already exists!");
+            
             var newNode = new BinarySearchTreeNode<V>
             {
                 Data = elementValue
             };
-            if (this.GetElementNode((T)newNode) != null)
-                throw new ArgumentException($"Node with value: {elementValue} already exists!");
-                
             if (this.Root == null)
                 this.Root = (T) newNode;
             else
@@ -55,7 +55,79 @@ namespace Trees
                     previousNode.LeftNode = newNode;          
                 else
                     previousNode.RightNode = newNode;
+
+                newNode.Parent = previousNode;
             }
+        }
+
+        // TODO: Refazer
+        public void RemoveValue(V value)
+        {
+            if (this.Root == null)
+                throw new ArgumentException($"Tree is already empty!");
+
+            T existingNode = (T)this.GetElementNodeByValue(value);
+            if (existingNode == null)
+                throw new ArgumentException($"Node with value {value} doesn't exist.");
+
+            if (existingNode.Parent == null)
+                throw new ArgumentException($"Can't remove root node.");
+
+            T parentNode = (T)existingNode.Parent;
+            T currentNode = existingNode;
+            // Existing node is a leaf
+            if (existingNode.LeftNode == null && existingNode.RightNode == null)
+            {
+                if (parentNode.LeftNode?.Data.CompareTo(currentNode.Data) == 0)
+                    parentNode.LeftNode = null;
+                else
+                    parentNode.RightNode = null;
+                return;
+            }
+            else if (currentNode.RightNode == null)
+            {
+                currentNode = (T)existingNode.LeftNode;
+                currentNode.Parent = existingNode.Parent;
+            }
+            else if (currentNode.LeftNode == null)
+            {
+                currentNode = (T)existingNode.RightNode;
+                currentNode.Parent = existingNode.Parent;
+            }
+            else
+            {
+                currentNode = (T)existingNode.LeftNode;
+                while(currentNode.RightNode != null)
+                    currentNode = (T)currentNode.RightNode;
+
+                if (currentNode.LeftNode != null)
+                    ((T)currentNode.Parent).RightNode = currentNode.LeftNode;
+                
+                currentNode.Parent = existingNode.Parent;
+                if (currentNode.Data.CompareTo(existingNode.LeftNode.Data) != 0)
+                    currentNode.LeftNode = existingNode.LeftNode;
+                if (currentNode.Data.CompareTo(existingNode.RightNode.Data) != 0)
+                    currentNode.RightNode = existingNode.RightNode;
+            }
+            this.ReplaceNode(existingNode, currentNode);
+        }
+
+        public void ReplaceNode(T existingNode, T currentNode)
+        {
+            T parent = (T)existingNode.Parent;
+            if (parent == null)
+                return;
+
+            if (existingNode.RightNode != null && existingNode.RightNode?.Data.CompareTo(currentNode.Data) != 0)
+                existingNode.RightNode.Parent = currentNode;
+
+            if (existingNode.LeftNode != null && existingNode.LeftNode?.Data.CompareTo(currentNode.Data) != 0)
+                existingNode.LeftNode.Parent = currentNode;
+
+            if (parent.LeftNode?.Data.CompareTo(existingNode.Data) == 0)
+                parent.LeftNode = currentNode;
+            else
+                parent.RightNode = currentNode;
         }
     }
 }
--- Trees/Program.cs
+++ Trees/Program.cs
@@ -31,24 +31,28 @@ namespace Trees
             System.Console.WriteLine($"Tree's max height is {maxHeight}");
 
             var binarySearchTree = new BinarySearchTree<BinarySearchTreeNode<int>, int>();
-            bool finishExecution = false;
-            // do
-            // {
-            //     System.Console.WriteLine("Insert element value");
-            //     string val = Console.ReadLine();
-            //     var elementValue = Convert.ToInt32(val);
-            //     binarySearchTree.InsertValue(elementValue);
-
-            //     System.Console.WriteLine("Press escape to finish execution");
-            //     finishExecution = Console.ReadKey().Key == ConsoleKey.Escape;
-            // }while(finishExecution==false);
             binarySearchTree.InsertValue(2);
             binarySearchTree.InsertValue(4);
             binarySearchTree.InsertValue(3);
             binarySearchTree.InsertValue(5);
+            binarySearchTree.InsertValue(10);
+            binarySearchTree.InsertValue(8);
+            binarySearchTree.InsertValue(9);
             binarySearchTree.InsertValue(1);
-            
-            var x = 1;
+            binarySearchTree.RemoveValue(1);
+            binarySearchTree.RemoveValue(4);
+            binarySearchTree.RemoveValue(5);
+            binarySearchTree.RemoveValue(3);
+            binarySearchTree.RemoveValue(8);
+            binarySearchTree.RemoveValue(10);
+            binarySearchTree.RemoveValue(9);
+            binarySearchTree.InsertValue(4);
+            binarySearchTree.InsertValue(3);
+            binarySearchTree.InsertValue(5);
+            binarySearchTree.InsertValue(1);
+            binarySearchTree.InsertValue(10);
+            binarySearchTree.InsertValue(8);
+            binarySearchTree.InsertValue(9);
         }
     }
 }
